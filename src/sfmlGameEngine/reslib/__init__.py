d48e1297721aa98ce6b2b3b5fd8c68cb8b1ff9c9
Madour <modar1999@gmail.com>
2020-03-11 14:30:08 +0100

    Better resource access. You can now iterate over Res , Res.Textures , Res.Maps and Res.Fonts

--- src/sfmlGameEngine/reslib/__init__.py
+++ src/sfmlGameEngine/reslib/__init__.py
@@ -3,13 +3,18 @@ from .resource_path import find_resource, RES_DIR
 
 
 class ResMeta(type):
-    """A Resource manager. Automatically loads all resources found in `assets` directory"""
-
-    ready = False
-    assets = resource_loader.Dir("assets")
-    assets._textures = resource_loader.Dir("textures")
-    assets._maps = resource_loader.Dir("maps")
-    assets._fonts = resource_loader.Dir("fonts")
+    """
+    To use the res manager, there are some naming convention you should follow:
+        - your files and folders should respect python variable naming rules
+        - your files and folders names should not start with an underscore
+        - " Textures " , " Maps " and " Fonts " are reserved. Avoid using those as a name.
+    """
+    _ready = False
+    _assets = resource_loader.Dir("assets")
+    _textures = resource_loader.Dir("textures")
+    _maps = resource_loader.Dir("maps")
+    _fonts = resource_loader.Dir("fonts")
+    _parent = None
 
     @property
     def Textures(cls):
@@ -23,27 +28,59 @@ class ResMeta(type):
     def Fonts(cls):
         return cls._fonts
 
+    @property
+    def is_ready(cls):
+        return cls._ready
+
     def __iter__(cls):
-        for x in cls.__dict__["assets"]:
+        for x in cls._assets:
             yield x
 
+    def values(cls):
+        return cls._assets.values()
+
+    def items(cls):
+        return cls._assets.items()
+
     def __getattr__(cls, item):
-        return getattr(cls.assets, item)
+        if item[0] == "_":
+            return cls.__dict__[item]
+        return getattr(cls._assets, item)
+
+    def __setattr__(cls, key, value):
+        if key[0] != "_":
+            raise AttributeError("You can not set attribute for Res class.")
+        else:
+            super().__setattr__(key, value)
+
+    def all_files(cls):
+        return cls._assets.all_files()
+
+    def print_tree(cls):
+        print(f"{RES_DIR}")
+        cls._assets.print_tree(indent=1)
 
 
 class Res(metaclass=ResMeta):
+    """
+    A Resource manager. Automatically loads all resources found in `assets` directory
+    To access a resource, you can use : `Res.folder_name.resource_name`
+    Res loads all assets recursively, so you can access nested directories this way :
+    `Res.folder1.folder2.foldeer3.resource_name`
+    Res.Textures contains all textures at the same level.
+    Res.Maps contains all tiled maps.
+    Res.Fonts contains all fonts.
+    """
     @classmethod
     def load(cls):
         """Load all resources into Res.
         You need to call this methode once at the start of your game (via GameEngine.load_resources() )
         if you want to use the resource manager.
         """
+        cls._assets._parent = cls
+        load_resources(cls._assets, find_resource(RES_DIR))
 
-        load_resources(cls.assets, find_resource(RES_DIR))
-        for asset in cls.assets.all_files():
-            if asset.__class__.__name__ == "TileMap":
-                asset.load()
-        cls.ready = True
+        for tile_map in cls.Maps.values():
+            tile_map.load()
 
-
-
+        cls._ready = True
--- src/sfmlGameEngine/reslib/resource_loader.py
+++ src/sfmlGameEngine/reslib/resource_loader.py
@@ -2,6 +2,7 @@ from sfml import sf
 import os
 from .resource_path import find_resource
 from . import tile_mapping as tm
+from typing import Dict, Union
 
 
 def load_resources(obj, path):
@@ -12,8 +13,8 @@ def load_resources(obj, path):
                 value = sf.Texture.from_file(find_resource(os.path.join(path, file)))
                 setattr(obj, filename, value)
                 res = obj
-                while res.parent:
-                    res = res.parent
+                while res._parent:
+                    res = res._parent
                 setattr(res._textures, filename, value)
 
             elif ext in [".ttf"]:
@@ -23,16 +24,16 @@ def load_resources(obj, path):
                 value.get_texture(32).smooth = False
                 setattr(obj, filename, value)
                 res = obj
-                while res.parent:
-                    res = res.parent
+                while res._parent:
+                    res = res._parent
                 setattr(res._fonts, filename, value)
 
             elif ext in [".tmx"]:
                 value = tm.TileMap(find_resource(os.path.join(path, file)))
                 setattr(obj, filename, value)
                 res = obj
-                while res.parent:
-                    res = res.parent
+                while res._parent:
+                    res = res._parent
                 setattr(res._maps, filename, value)
 
         elif os.path.isdir(os.path.join(path, file)):
@@ -40,29 +41,38 @@ def load_resources(obj, path):
             load_resources(dir, os.path.join(path, file))
             setattr(obj, filename, dir)
 
+
 class Dir:
     def __init__(self, name, parent = None):
-        self.__dict__["name"] = name
-        self.__dict__["parent"] = parent
-        self.__dict__["data"] = {}
+        self._name = name
+        self._parent = parent
+        self._data : Dict[str, Union[Dir, sf.Texture, tm.TileMap, sf.Font]] = {}
 
     def __getattr__(self, item):
-        if item in self.__dict__["data"]:
-            return self.__dict__["data"][item]
-        raise AttributeError(f"file or directory '{item}' not found in directory '{self.name}'")
+        if item[0] == '_':
+            return self.__dict__[item]
+        if item in self._data:
+            return self._data[item]
+        raise AttributeError(f"File or directory '{item}' not found in directory '{self.name}'")
 
     def __setattr__(self, key, value):
         if key not in vars(dict).keys():
-            self.__dict__["data"][key] = value
+            if key[0] == '_':
+                self.__dict__[key] = value
+            else:
+                self._data[key] = value
         else:
             raise AttributeError(f"{key} is not a valid key")
 
     def __iter__(self):
-        for key in self.__dict__["data"].keys():
+        for key in self._data.keys():
             yield key
 
     def values(self):
-        return self.__dict__["data"].values()
+        return self._data.values()
+
+    def items(self):
+        return self._data.items()
 
     def all_files(self):
         for file in self.values():
@@ -72,3 +82,18 @@ class Dir:
             else:
                 yield file
 
+    def print_tree(self, indent=0):
+        items = [i for i in self.items()]
+        dirs = [d for d in items if isinstance(d[1], Dir)]
+        dirs.sort(key=lambda x:x[0])
+        files = [f for f in items if not isinstance(f[1], Dir)]
+        files.sort(key=lambda x:x[0])
+
+        for name, val in dirs:
+            print(" " * 4 * indent, end='')
+            print(f"|_ {name} : {type(val).__name__}")
+            val.print_tree(indent=indent + 1)
+
+        for name, val in files:
+            print(" " * 4 * indent, end='')
+            print(f"{name} : {type(val).__name__}")
