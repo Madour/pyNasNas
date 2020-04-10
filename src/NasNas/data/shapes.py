63b810fc497b2daba54b2b26c5b1a2d419922d10
Madour <modar1999@gmail.com>
2020-04-10 19:06:17 +0200

    Added LineShape

--- src/NasNas/data/shapes.py
+++ src/NasNas/data/shapes.py
@@ -6,8 +6,72 @@ from ..utils import to_Vector2
 from typing import Union, Tuple
 
 
+class LineShape(sf.Drawable):
+    def __init__(self, *args: Union[Tuple[float, float], sf.Vector2]):
+        super().__init__()
+        self._points = []
+        self._position = sf.Vector2(0, 0)
+        self._vertexarray = sf.VertexArray(sf.PrimitiveType.LINES_STRIP)
+        self._color = sf.Color.WHITE
+        for arg in args:
+            self._points.append(to_Vector2(arg))
+        self.update()
+
+    @property
+    def point_count(self) -> int:
+        return len(self._points)
+
+    @property
+    def position(self) -> sf.Vector2:
+        return self._position
+
+    @position.setter
+    def position(self, pos: Union[Tuple[float, float], sf.Vector2]):
+        self._position = to_Vector2(pos)
+        self.update(offset=True)
+
+    @property
+    def color(self) -> sf.Color:
+        return self._color
+
+    @color.setter
+    def color(self, color: sf.Color):
+        self._color = color
+        self.update(color=True)
+
+    def add_point(self, point: Union[Tuple[float, float], sf.Vector2]):
+        self._points.append(to_Vector2(point))
+        self.update(append=True)
+
+    def remove_point(self, index: int):
+        if index >= len(self._points):
+            raise IndexError(f"Point number {index} out of range.")
+        else:
+            self._points.pop(index)
+            self.update()
+
+    def update(self, append=False, color=False, offset=False):
+        if append:
+            self._vertexarray.resize(self.point_count)
+            self._vertexarray.append(sf.Vertex(position=self._points[-1], color=self._color))
+        if color:
+            for i in range(self.point_count):
+                self._vertexarray[i].color = self._color
+        if offset:
+            for vertex in self._vertexarray:
+                vertex.position += self._position
+        if not color and not append and not offset:
+                self._vertexarray.resize(self.point_count)
+                for i in range(self.point_count):
+                    self._vertexarray[i].position = self._points[i]
+                    self._vertexarray[i].color = self._color
+
+    def draw(self, target, states):
+        target.draw(self._vertexarray, states)
+
+
 class EllipseShape(sf.ConvexShape):
-    def __init__(self, radius: Union[Tuple[int, int], sf.Vector2]):
+    def __init__(self, radius: Union[Tuple[float, float], sf.Vector2]):
         super().__init__()
         self._radius = to_Vector2(radius)
         self.point_count = 30
